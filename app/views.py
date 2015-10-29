515fab53dde4212e60e3bc8ddd661c19df5e21e9
mara gatchalian <maragatchalian@github.com>
2015-10-29 11:08:49 +0800

    Control Statements and Loops in Templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Mara'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': '@ericjandale'}, 
+            'body': 'I am a Marine Engineer!' 
+        },
+        { 
+            'author': {'nickname': '@marsgatchalian'}, 
+            'body': 'I am inlove with a Marine Engineer!' 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
