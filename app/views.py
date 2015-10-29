ea89271708247dd101386f3ce1bd34101a6eeb30
mara gatchalian <maragatchalian@github.com>
2015-10-29 11:23:02 +0800

    Login Form

--- app/views.py
+++ app/views.py
@@ -1,21 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Mara'}  # fake user
-    posts = [  # fake array of posts
-        { 
-            'author': {'nickname': '@ericjandale'}, 
-            'body': 'I am a Marine Engineer!' 
-        },
-        { 
-            'author': {'nickname': '@marsgatchalian'}, 
-            'body': 'I am inlove with a Marine Engineer!' 
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+# index view function suppressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
