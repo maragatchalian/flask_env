f34f309273b3695ff63cc74fe8f47e43f1a51b83
mara gatchalian <maragatchalian@github.com>
2015-11-02 16:47:58 +0800

    Send Errors locally on Another Terminal

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,5 +14,15 @@ lm = LoginManager()
 lm.init_app(app)
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 lm.login_view = 'login'
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 from app import views, models
--- config.py
+++ config.py
@@ -13,3 +13,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+# mail server settings
+MAIL_SERVER = 'localhost'#smtp.mail.yahoo.com'
+MAIL_PORT = 2525
+MAIL_USERNAME = None #os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = None #os.environ.get('MAIL_PASSWORD')
+
+# administrator list
+ADMINS = ['marsgatchalian@yahoo.com']
