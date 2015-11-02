e29192b2403780814290022e03d42beaa870ee7d
mara gatchalian <maragatchalian@github.com>
2015-11-02 15:46:00 +0800

    Revert "Nickname bug fixing and Fake email trial"
    This reverts commit 423122e0a447be4184fcb2cf711e1128ad8ccd9c.

--- app/views.py
+++ app/views.py
@@ -27,16 +27,13 @@ def after_login(resp):
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
     user = User.query.filter_by(email=resp.email).first()
-   
     if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-        nickname = User.make_unique_nickname(nickname) #added
-        user = User(nickname = nickname, email = resp.email)
+        user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
-
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
@@ -99,8 +96,7 @@ def user(nickname):
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-    form = EditForm(g.user.nickname)
-    # form = EditForm() - previous
+    form = EditForm()
     if form.validate_on_submit():
         g.user.nickname = form.nickname.data
         g.user.about_me = form.about_me.data
@@ -112,12 +108,3 @@ def edit():
         form.nickname.data = g.user.nickname
         form.about_me.data = g.user.about_me
     return render_template('edit.html', form=form)
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
--- tmp/associations/https-api.screenname.aol.com-BUbSb4QlU9PGPbUUYLeiWlAagtw-YcSmkNe1OUvNAGYw6.L65mPEh.s
+++
@@ -1,6 +0,0 @@
-version:2
-handle:e7f757da811211e59b4700163e044d3a
-secret:510ltTd2iOssOaUFgVfTc/UBhTg=
-issued:1446435392
-lifetime:86398
-assoc_type:HMAC-SHA1
