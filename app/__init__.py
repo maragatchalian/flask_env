01b697d29822de4993dd70611d7402aeccabce80
mara gatchalian <maragatchalian@github.com>
2015-11-10 11:45:42 +0800

    Ago function

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,13 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from .momentjs import momentjs
 
 app = Flask(__name__)
 mail = Mail(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+app.jinja_env.globals['momentjs'] = momentjs
 
 lm = LoginManager()
 lm.init_app(app)
